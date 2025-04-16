dbb4b0a3389d9ebde1b74f7431e2de49ff88d525
ThanhSon00 <phanson999999@gmail.com>
2025-04-16 06:39:42 -0400

    Add test and code for product update function

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,24 +182,14 @@ class TestProductRoutes(TestCase):
 
     def test_update_product(self):
         """It should Update an existing Product"""
-        # create a product to update
         test_product = ProductFactory()
-        # send a self.client.post() request to the BASE_URL with a json payload of test_product.serialize()
-        response = self.client.post(f"{BASE_URL}", data=test_product.serialize())
-        # assert that the resp.status_code is status.HTTP_201_CREATED
-        self.assertEqual(response.status, status.HTTP_201_CREATED)
-        # UPDATE THE PRODUCT
-        # get the data from resp.get_json() as new_product
+        response = self.client.post(f"{BASE_URL}", json=test_product.serialize(), content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         new_product = response.get_json()
-        # change new_account["description"] to unknown
         new_product["description"] = 'unknown'
-        # send a self.client.put() request to the BASE_URL with a json payload of new_product
-        response = self.client.put(f"{BASE_URL}/{new_product.id}", data=new_product)
-        # assert that the resp.status_code is status.HTTP_200_OK
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # get the data from resp.get_json() as updated_product
         updated_product = response.get_json()
-        # assert that the updated_product["description"] is whatever you changed it to
         self.assertEqual(updated_product["description"], 'unknown')
 
     ######################################################################
